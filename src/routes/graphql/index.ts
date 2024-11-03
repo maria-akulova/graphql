dc9d7d56aab6833438d219564c7e40c9a67ade7a
maria.akulova <maria.akulova.dragon@gmail.com>
2024-11-03 16:01:19 +0100

    feat: test-rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -4,8 +4,9 @@ import {
   gqlResponseSchema,
   schema,
 } from "./schemas.js";
-import { graphql } from "graphql";
+import { graphql, parse, validate } from "graphql";
 import { rootValue } from "./resolvers/main.js";
+import depthLimit from "graphql-depth-limit";
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -18,6 +19,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const ast = parse(req.body.query);
+      const validationErrors = validate(schema, ast, [depthLimit(5)]);
+      if (validationErrors.length) {
+        return { errors: validationErrors };
+      }
+
       return graphql({
         schema,
         source: req.body.query,
